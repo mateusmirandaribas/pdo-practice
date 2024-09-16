d191994869543f8889a2644d5cd7507f73d3f05b
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-09-16 17:56:30 -0300

    feat: adding create students table migration

---
+++ src/Database/Migrations/create_students_table.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../../../index.php';
+
+use src\Repository\SQLiteConnection;
+
+try {
+    $sqliteConnection = new SQLiteConnection();
+    $sqliteConnection->pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+
+    echo "Tabela criada com sucesso!";
+} catch (Exception $execption) {
+    echo $execption->getMessage() . PHP_EOL . $execption->getCode();
+}
