28f1b09fc8366686bf9bb8bdb432094fcddabc15
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-09-17 12:02:14 -0300

    feat: adding script for select all students

---
+++ scripts/select-students-script.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../index.php';
+
+use src\Repository\SQLiteConnection;
+use src\Model\Student as StudentModel;
+
+try {
+    $sqliteConnection = new SQLiteConnection();
+    $studentModel = new StudentModel($sqliteConnection);
+
+    $studentList = $studentModel->selectAllStudents();
+    var_dump($studentList);
+} catch (Exception $execption) {
+    echo $execption->getMessage() . PHP_EOL . $execption->getCode();
+}
