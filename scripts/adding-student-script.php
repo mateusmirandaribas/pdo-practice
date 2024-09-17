c5b1fcb17447051339fe711f32d8d61468253d9e
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-09-16 21:05:13 -0300

    feat: adding script for adding student

---
+++ scripts/adding-student-script.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../index.php';
+
+use src\Student as StudentObject;
+use src\Repository\SQLiteConnection;
+use src\Model\Student as StudentModel;
+
+try {
+    $sqliteConnection = new SQLiteConnection();
+    $studentModel = new StudentModel($sqliteConnection);
+
+    $student = new StudentObject(
+        null,
+        'Jon',
+        '2001-01-01'
+    );
+
+    $studentModel->store($student);
+} catch (Exception $execption) {
+    echo $execption->getMessage() . PHP_EOL . $execption->getCode();
+}
