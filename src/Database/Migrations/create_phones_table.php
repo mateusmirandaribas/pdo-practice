3c80ff52f1b2a18cc1b51ae3a3439a2388665418
mateusmirandaribas <contato.mateusmribas@gmail.com>
2024-10-10 17:37:01 -0300

    feat: adding create phones table migration

---
+++ src/Database/Migrations/create_phones_table.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../../../index.php';
+
+use src\Infrastructure\SQLiteConnection;
+
+try {
+    $sqliteConnection = new SQLiteConnection();
+    $sqliteConnection->pdo->exec(
+        'CREATE TABLE phones (
+            id INTEGER PRIMARY KEY,
+            area_code TEXT,
+            number TEXT,
+            student_id INTEGER,
+            FOREIGN KEY(student_id) REFERENCES students(id)
+        );'
+    );
+
+    echo "Table created successfully!";
+} catch (Exception $execption) {
+    echo $execption->getMessage() . PHP_EOL . $execption->getCode();
+}
